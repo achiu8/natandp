440e7f5572ef28e2f3f282241914137081ac76ba
kprakobkit <kprakobkit@gmail.com>
2015-07-25 15:06:52 -0700

    [Peter] - Setup angular

---
+++ public/scripts/app.js
@@ -0,0 +1,19 @@
+var natAndP = angular.module('natAndP', ['ngRoute']);
+
+var routes = function($routeProvider, $locationProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'templates/main.html',
+    controller: mainController
+  });
+}
+
+routes.$inject = ['$routeProvider'];
+
+var mainController = function($scope) {
+  $scope.text = "hello world";
+};
+
+mainController.$inject = ['$scope'];
+
+natAndP.controller('mainController', mainController);
+natAndP.config(routes);
