25458ff7184976ea1b85f4cf2b08e14182e8f899
andyc <andyc@wealthfront.com>
2015-07-25 20:40:09 -0700

    add attending to words helper

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,8 @@
 module AdminHelper
+  def attending_to_words(attending)
+    case attending
+    when :neither, :both then "attending #{attending}"
+    else                      "attending #{format_attending(attending)}"
+    end
+  end
 end
