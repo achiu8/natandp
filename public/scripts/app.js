41c4cf91d123bdbe1f8d005b52b54dc5df93ca37
kprakobkit <kprakobkit@gmail.com>
2015-07-26 13:19:51 -0700

    Add rsvp directive.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,19 @@
 define([
   'controller/main_controller',
+  'directives/rsvp',
   'routes',
   'angular',
   'angular-route'
 ], function(
   mainController,
+  rsvp,
   routes,
   angular
 ) {
   var natAndP = angular.module('natAndP', ['ngRoute']);
 
   natAndP.controller('mainController', mainController);
+  natAndP.directive('rsvp', rsvp);
   natAndP.config(routes);
 
   natAndP.init = function() {
