7c0aab4ac509bc40dc3100be96883dc4d01dd1d8
kprakobkit <kprakobkit@gmail.com>
2015-07-26 14:39:43 -0700

    add parser for form response. sloppy, but it works...?

--- public/scripts/controller/rsvp_controller.js
+++ public/scripts/controller/rsvp_controller.js
@@ -1,9 +1,42 @@
 define([], function() {
   var rsvpController = function($scope) {
     $scope.title = "RSVP Y'all";
+    $scope.guest = {};
+    $scope.attendingOptions = [
+      {value: true, label: 'in person'},
+      {value: false, label: 'in spirit'}
+    ]
+
     $scope.submitForm = function() {
-      console.log('Submitt form...');
-      console.log($scope.guest);
+      var parsedResponse = parseRsvpFormResponse($scope.guest);
+      console.log(parsedResponse);
+
+      // fire the torpedoes!!!!!
+    }
+
+    function parseRsvpFormResponse(response) {
+      return {
+        first_name: response.firstName,
+        last_name: response.lastName,
+        plus_ones: response.plus_ones,
+        phone: response.phone,
+        email: response.email,
+        attending: parseAttendingResponse(response)
+      }
+    }
+
+    function parseAttendingResponse(response) {
+      if (response.willAttend) {
+        if (response.attendingSaratoga && response.attendingThailand) {
+          return 'both';
+        } else if (response.attendingSaratoga) {
+          return 'saratoga_springs';
+        } else if (response.attendingThailand) {
+          return 'thailand';
+        }
+      }
+
+      return 'neither';
     }
   };
 
