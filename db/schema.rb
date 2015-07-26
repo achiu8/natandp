95bfe933e779fa3ac368f75e2a456eeb63e217a3
kprakobkit <kprakobkit@gmail.com>
2015-07-25 19:14:02 -0700

    [Peter] - Add configuration for postgres to deploy on heroku.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150725191701) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "guests", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
