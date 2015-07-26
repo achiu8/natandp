62ee69c640e69b4ca73c163a380c8834a955c654
kprakobkit <kprakobkit@gmail.com>
2015-07-26 14:59:17 -0700

    Add form post to create guest in DB.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,18 @@
 class MainController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def create_guest
-    @guest = Guest.new(params[:guest])
+    @guest = Guest.new(guest_params)
     if @guest.save
       render json: { success: true }, status: 200
     else
       render json: { errors: @guest.errors.full_messages }, status: 400
     end
+  end
+
+  private
+
+  def guest_params
+    params.require(:guest).permit!
   end
 end
