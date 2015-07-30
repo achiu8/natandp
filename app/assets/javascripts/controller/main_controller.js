32c0371312f6a8ef61dbf94a716c8fb485957802
andyc <andyc@wealthfront.com>
2015-07-30 01:48:43 -0700

    add slideout mobile menu

--- app/assets/javascripts/controller/main_controller.js
+++ app/assets/javascripts/controller/main_controller.js
@@ -1,9 +1,29 @@
 var mainController = function($scope, $location, $anchorScroll) {
   $scope.text = "hello world";
+  $scope.$body = $(document.body);
+  $scope.$navBar = $('.nav-bar');
+  $scope.$navBarIcon = $('.nav-bar-mobile');
+  $scope.navBarMobileShown = false;
 
   $scope.goTo = function(id) {
     $location.hash(id);
     $anchorScroll();
+
+    if ($scope.navBarMobileShown) {
+      $scope.toggleNav();
+    }
+  };
+
+  $scope.toggleNav = function() {
+    $scope.navBarMobileShown = !$scope.navBarMobileShown;
+    $scope.$body.toggleClass('scroll-lock', $scope.navBarMobileShown);
+    resetNavPosition();
+  };
+
+  function resetNavPosition() {
+    var navBarIconNewTop = $scope.navBarMobileShown ? window.scrollY : 0 ;
+    $scope.$navBar.css({ top: window.scrollY, position: 'fixed' });
+    $scope.$navBarIcon.css({ top: navBarIconNewTop + 10, position: 'fixed' });
   }
 };
 
