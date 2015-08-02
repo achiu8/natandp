2fb95b189b8d1b3c616bff0af30db990875bc1cb
kprakobkit <kprakobkit@gmail.com>
2015-08-01 20:48:13 -0700

    Add preview to picture-carousel.

--- app/assets/javascripts/directives/picture_section.js
+++ app/assets/javascripts/directives/picture_section.js
@@ -3,7 +3,20 @@ var pictureSectionDirective = function() {
     templateUrl: 'templates/directives/picture_section.html.erb',
     link: function(elem) {
       $('.picture-carousel').slick({
-        dots: true
+        fade: true,
+        speed: 500,
+        cssEase: 'linear',
+        arrows: false,
+        asNavFor: '.picture-carousel-nav'
+      });
+
+      $('.picture-carousel-nav').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+        asNavFor: '.picture-carousel'
       });
     }
   }
