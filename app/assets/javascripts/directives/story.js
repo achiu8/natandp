7fb557f37d6f97201e7346c3a48406d2fa23002e
kprakobkit <kprakobkit@gmail.com>
2015-08-08 22:32:32 -0700

    Add shadow to content and add prev/next button (not functioning).

--- app/assets/javascripts/directives/story.js
+++ app/assets/javascripts/directives/story.js
@@ -3,12 +3,12 @@ var storyDirective = function() {
     templateUrl: 'templates/directives/story.html',
     link: function(scope, element, attr) {
       scope.story = {
-        'How We Met': 'cccontent - how we metcontenccontent - how we metcontencontent - how we metcontent - how we metcontent - how we metcontent - how we metcontent - how we metcontent - how we metontent - how we met',
-        'Started Dating': 'ccontent - started datingcontent - started datingcoccontent - how we metcontenntent - started datingcontent - started datingcontent - started datingcontent - started datingontent - started dating',
-        'Moved in Together': 'ccontent - moved in togethercontent - moved in togethercontent - moved in togethercontent - moved in togetherontent - ccontent - how we metcontenccontent - how we metcontenmoved in together',
-        'Welcome Butter': 'cccontent - welcome buttercontent - welcome buttercontent - welcome buttercontent - welcome butterontent - welcome buttercontent - welcome buttercontent - welcome buttercontent - welcome buttercontent - welcome butterontent - welcome butter',
-        'Proposed': 'cccontent - proposedcontent - proposedcontent - proposedcontent - proposedontent - proposedcontent - proposedcontent - proposedcontent - proposedcontent - proposedontent - proposed',
-        'Wedding': 'cccontent - wedding content - wedding content - wedding content - wedding ontent - wedding content - ccontent - how we metcontenwedding content - wedding content - wedding content - wedding ontent - wedding '
+        'How We Met'        : 'cccontent - how we metcontenccontent - how we metcontencontent - how we metcontent - how we metcontent - how we metcontent - how we metcontent - how we metcontent - how we metontent - how we met',
+        'Started Dating'    : 'ccontent - started datingcontent - started datingcoccontent - how we metcontenntent - started datingcontent - started datingcontent - started datingcontent - started datingontent - started dating',
+        'Moved in Together' : 'ccontent - moved in togethercontent - moved in togethercontent - moved in togethercontent - moved in togetherontent - ccontent - how we metcontenccontent - how we metcontenmoved in together',
+        'Welcome Butter'    : 'cccontent - welcome buttercontent - welcome buttercontent - welcome buttercontent - welcome butterontent - welcome buttercontent - welcome buttercontent - welcome buttercontent - welcome buttercontent - welcome butterontent - welcome butter',
+        'Proposed'          : 'cccontent - proposedcontent - proposedcontent - proposedcontent - proposedontent - proposedcontent - proposedcontent - proposedcontent - proposedcontent - proposedontent - proposed',
+        'Wedding'           : 'cccontent - wedding content - wedding content - wedding content - wedding ontent - wedding content - ccontent - how we metcontenwedding content - wedding content - wedding content - wedding ontent - wedding '
       }
       scope.milestones = Object.keys(scope.story);
       scope.content = scope.story['How We Met'];
