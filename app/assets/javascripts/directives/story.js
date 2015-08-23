f4e29be41d2b42604b16726c1b3ce6ba2cbb4d51
kprakobkit <kprakobkit@gmail.com>
2015-08-23 12:20:45 -0700

    Refactor to use image sprites.

--- app/assets/javascripts/directives/story.js
+++ app/assets/javascripts/directives/story.js
@@ -4,23 +4,23 @@ var storyDirective = function($timeout) {
     link: function(scope, element, attr) {
       scope.stories = {
         'HOW WE MET': {
-          backgroundUrl: 'images/how-we-met.png',
+          className: 'how-we-met',
           story: "Natalie and Peter were introduced at work. It was Natalie's first interview with the company, and she met Peter while he was playing with a yoga ball. She thought it was the cutest thing in the world!"
         },
         'DATING': {
-          backgroundUrl: 'images/started-dating.png',
+          className: 'started-dating',
           story: 'They started dating and spent their time binge watching Netflix, taking trips to Tahoe, and sharing their love for food. They also cooked for each other, Peter would cook and Natalie baked...and they fell in love.'
         },
         'BUTTER': {
-          backgroundUrl: 'images/butter.png',
+          className: 'butter',
           story: "Natalie always wanted a dog, so for her birthday, Peter took home Butter, an italian greyhound, and they welcomed her to the family. She's a one of a kind dog and they couldn't be happier to have brought Butter into their lives."
         },
         'PROPOSAL': {
-          backgroundUrl: 'images/proposed.png',
+          className: 'proposal',
           story: "On June 27, 2015, Peter took Natalie out to a fancy dinner...little did she know what was going to happen. When they got home, Peter got down on one knee and asked Natalie if she would spend the rest of her life with him. She said yes!"
         },
         'WEDDING': {
-          backgroundUrl: 'images/wedding.png',
+          className: 'wedding',
           story:"Natalie and Peter will have two weddings, the first one is in California and second is in Bangkok. They couldn't be happier and cannot wait to celebrate (and party) with their friends and family."
         }
       }
@@ -30,34 +30,36 @@ var storyDirective = function($timeout) {
       scope.showMainStory = false;
       scope.showGreeting = false;
       scope.showIntroStory = false;
+      var currentStory = "how-we-met";
 
       scope.showContentFor = function(milestone) {
         scope.selectedMilestone = milestone;
         scope.selectedContent = scope.stories[milestone].story;
-        setBackground(scope.stories[milestone].backgroundUrl);
+        setBackground(scope.stories[milestone].className);
       }
 
       scope.isSelected = function(milestone) {
         return scope.selectedMilestone === milestone;
       }
 
-      var setBackground = function(url) {
-        var backgroundUrl = "url(" + url + ")"
-        $('.story').css('background-image', backgroundUrl);
+      var setBackground = function(className) {
+        $('.story-background').removeClass(currentStory);
+        $('.story-background').addClass(className);
+        currentStory = className;
       }
 
       scope.showStory = function() {
         scope.showMainStory = true;
         scope.showIntroStory = false;
         scope.showGreeting = false;
-        setBackground('images/how-we-met.png');
+        setBackground('how-we-met');
       };
 
       $timeout(function() {
-        setBackground('images/hi-swing.png');
         $timeout(function() {
           $('.content').addClass("loaded");
           $timeout(function() {
+            setBackground('hi-swing');
             scope.showIntroStory = true;
             scope.showGreeting = true;
           }, 750);
