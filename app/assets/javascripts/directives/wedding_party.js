7513b8a236aa97d2a89af2e11a87f96617db832d
kprakobkit <kprakobkit@gmail.com>
2015-08-25 21:10:39 -0700

    Add full names to wedding partiers.

--- app/assets/javascripts/directives/wedding_party.js
+++ app/assets/javascripts/directives/wedding_party.js
@@ -1,21 +1,57 @@
 var weddingPartyDirective = function() {
   var controller = ['$scope', function($scope) {
     $scope.groomsmen = [
-      'geoff',
-      'guru',
-      'baummer',
-      'ben',
-      'andy',
-      'john'
+      {
+        shortName: 'geoff',
+        fullName: 'Geoff Consolacion',
+      },
+      {
+        shortName: 'guru',
+        fullName: 'Guru Velasco',
+      },
+      {
+        shortName: 'baummer',
+        fullName: 'James Baummer',
+      },
+      {
+        shortName: 'ben',
+        fullName: 'Ben Pham',
+      },
+      {
+        shortName: 'Andy',
+        fullName: 'Andy Chiu',
+      },
+      {
+        shortName: 'john',
+        fullName: 'John Lin',
+      }
     ]
 
     $scope.bridesmaids = [
-      'kim',
-      'leah',
-      'amy',
-      'lisa',
-      'carla',
-      'ainsley'
+      {
+        shortName: 'kim',
+        fullName: 'Kim Hoang',
+      },
+      {
+        shortName: 'leah',
+        fullName: 'Leah Hernandez',
+      },
+      {
+        shortName: 'amy',
+        fullName: 'Amy Dong',
+      },
+      {
+        shortName: 'lisa',
+        fullName: 'Lisa Nguyen',
+      },
+      {
+        shortName: 'carla',
+        fullName: 'Carla Monsalud',
+      },
+      {
+        shortName: 'ainsley',
+        fullName: 'Ainsley Reyes',
+      }
     ]
 
     $scope.showModal = false;
